6f64c73c40c9bffc5f088b6cbac77a99158c0d46
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-22 20:16:33 +0600

    Added tabbar like button in the bottom

---
+++ Farvima/Pharma Home/PharmaHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PharmaHomeViewController.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/22/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PharmaHomeViewController : UIViewController
+
+@end
---
+++ Farvima/Pharma Home/PharmaHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  PharmaHomeViewController.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/22/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "PharmaHomeViewController.h"
+
+@interface PharmaHomeViewController ()
+
+@end
+
+@implementation PharmaHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
