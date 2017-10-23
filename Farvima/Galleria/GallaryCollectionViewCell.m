24627200248c0cf2e1dfc3dc59d126edf1fa4ff4
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-23 23:27:28 +0600

    Added gallary page design

---
+++ Farvima/Galleria/GallaryCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  GallaryCollectionViewCell.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/23/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "GallaryCollectionViewCell.h"
+
+@implementation GallaryCollectionViewCell
+
+@end
