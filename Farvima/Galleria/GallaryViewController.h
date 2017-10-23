56919089754ddd6e49291a15244921c049e91576
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-23 22:49:04 +0600

    Added navigattion bar

---
+++ Farvima/Galleria/GallaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GallaryViewController.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/23/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GallaryViewController : UIViewController
+
+@end
---
+++ Farvima/Galleria/GallaryViewController.m
@@ -0,0 +1,42 @@
+//
+//  GallaryViewController.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/23/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "GallaryViewController.h"
+
+@interface GallaryViewController ()
+- (IBAction)backButtonAction:(id)sender;
+
+@end
+
+@implementation GallaryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)backButtonAction:(id)sender {
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
