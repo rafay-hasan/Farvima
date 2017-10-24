835ec8f41597af4ea9adade2fa57ea8d08b107bd
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-24 22:18:55 +0600

    Added offer table view cel

---
+++ Farvima/Offerete/OfferTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  OfferTableViewCell.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/24/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfferTableViewCell : UITableViewCell
+
+@end
---
+++ Farvima/Offerete/OfferTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  OfferTableViewCell.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/24/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "OfferTableViewCell.h"
+
+@implementation OfferTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
