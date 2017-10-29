e6cdd06b9609e467fc88e41b3ea59fd9db73d000
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-29 12:28:49 +0600

    Added product search page design

---
+++ Farvima/Product search/ProductSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductSearchViewController.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/29/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductSearchViewController : UIViewController
+
+@end
