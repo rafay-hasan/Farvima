4965bd8d48a7c01692fe0359e7b851b37d4fe000
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-10-29 12:41:18 +0600

    Added Pod file, iQKeyboard manager, enabled it.

--- Farvima/AppDelegate.m
+++ Farvima/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "IQKeyboardManager.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [IQKeyboardManager sharedManager].enable = YES;
 
 //    UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
 //    MasterViewController *controller = (MasterViewController *)masterNavigationController.topViewController;
