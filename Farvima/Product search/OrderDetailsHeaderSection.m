b826cc0e28ad7958974225d5aa125464da6d323c
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-07 20:45:56 +0600

    Added message and notification button action in Busket page

---
+++ Farvima/Product search/OrderDetailsHeaderSection.m
@@ -0,0 +1,21 @@
+//
+//  OrderDetailsHeaderSection.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/30/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "OrderDetailsHeaderSection.h"
+
+@implementation OrderDetailsHeaderSection
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Farvima/Product search/OrderDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderDetailsViewController.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 10/30/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderDetailsViewController : UIViewController
+
+@end
