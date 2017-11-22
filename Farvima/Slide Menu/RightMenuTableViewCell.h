2b9b2cc6b5b5a6dd5c96efbab892cd8419ed1b57
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-22 06:37:55 +0600

    Added rightSlide menu tableview

---
+++ Farvima/Slide Menu/RightMenuTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  RightMenuTableViewCell.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/22/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RightMenuTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *categoryNameLabel;
+
+@end
---
+++ Farvima/Slide Menu/RightMenuTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RightMenuTableViewCell.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/22/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "RightMenuTableViewCell.h"
+
+@implementation RightMenuTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
