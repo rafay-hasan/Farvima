17e70066253e06283a656feec6625f5cb4888f11
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-23 22:53:10 +0600

    Added offer page list/grid orientation.

---
+++ Farvima/Product search/SearchResultTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  SearchResultTableViewCell.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/23/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "SearchResultTableViewCell.h"
+
+@implementation SearchResultTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
