34c78b649a1763ef6f9484eb8e7edc5546d06f13
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-25 15:45:08 +0600

    Added SearchbyAddress and SearchByLocation view controller page.

---
+++ Farvima/Search by address/SearchByAddressViewController.m
@@ -0,0 +1,37 @@
+//
+//  SearchByAddressViewController.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/25/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "SearchByAddressViewController.h"
+
+@interface SearchByAddressViewController ()
+
+@end
+
+@implementation SearchByAddressViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Farvima/Search by location/SearchByLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchByLocationViewController.h
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/25/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchByLocationViewController : UIViewController
+
+@end
