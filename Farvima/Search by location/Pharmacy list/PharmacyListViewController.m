0ae115924ec7442629b7cf3993ef782087cdac62
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-25 16:37:51 +0600

    Added pharmacy list page.

---
+++ Farvima/Search by location/Pharmacy list/PharmacyListViewController.m
@@ -0,0 +1,43 @@
+//
+//  PharmacyListViewController.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/25/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "PharmacyListViewController.h"
+
+@interface PharmacyListViewController ()
+
+- (IBAction)backButtonAction:(id)sender;
+
+
+@end
+
+@implementation PharmacyListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)backButtonAction:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
