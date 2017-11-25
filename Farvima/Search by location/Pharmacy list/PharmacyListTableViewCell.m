5d4cb0c0887ea0070d0d61d024fffbf38f13712d
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2017-11-25 17:17:52 +0600

    Added Pharmacy list tableview.

---
+++ Farvima/Search by location/Pharmacy list/PharmacyListTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  PharmacyListTableViewCell.m
+//  Farvima
+//
+//  Created by Rafay Hasan on 11/25/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import "PharmacyListTableViewCell.h"
+
+@implementation PharmacyListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- Farvima/Search by location/Pharmacy list/PharmacyListViewController.m
+++ Farvima/Search by location/Pharmacy list/PharmacyListViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "PharmacyListViewController.h"
+#import "PharmacyListTableViewCell.h"
 
-@interface PharmacyListViewController ()
+@interface PharmacyListViewController () <UITableViewDelegate,UITableViewDataSource>
 
 - (IBAction)backButtonAction:(id)sender;
 
@@ -36,6 +37,36 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 15;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    PharmacyListTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PharmacyListCell" forIndexPath:indexPath];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UIView *headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor clearColor];
+    return headerView;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView *footerView = [[UIView alloc] init];
+    footerView.backgroundColor = [UIColor colorWithRed:6.0/255.0 green:39.0/255.0 blue:156.0/255.0 alpha:1];
+    return footerView;
+}
 
 - (IBAction)backButtonAction:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
